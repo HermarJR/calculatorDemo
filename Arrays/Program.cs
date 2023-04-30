779798a7363c94ac4206f758caa881b3d977ca36
Hermar <Hermar@LAPTOP-7BL7JTC7>
2023-04-30 19:59:12 +0800

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,63 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("**********");
+        Console.WriteLine("Calculator");
+        Console.WriteLine("**********");
+
+        bool again = true;
+
+        while (again)
+        {
+            float firstNum;
+            float secondNum;
+            float result;
+            string op = "";
+
+            Console.Write("Input first number: ");
+            firstNum = float.Parse(Console.ReadLine());
+            Console.Write("Input second number: ");
+            secondNum = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose your Operator: ADD(+), SUB(-), DIV(/), MUL(*) ");
+            op = Console.ReadLine();
+
+            switch (op)
+            {
+                case "+":
+                    result = firstNum + secondNum;
+                    Console.WriteLine(firstNum + " + " + secondNum + " = " + result);
+                    break;
+                case "-":
+                    result = firstNum + secondNum;
+                    Console.WriteLine(firstNum + " - " + secondNum + " = " + result);
+                    break;
+                case "/":
+                    result = firstNum + secondNum;
+                    Console.WriteLine(firstNum + " / " + secondNum + " = " + result);
+                    break;
+                case "*":
+                    result = firstNum + secondNum;
+                    Console.WriteLine(firstNum + " * " + secondNum + " = " + result);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Operator");
+                    break;
+            }
+
+            Console.WriteLine("would you like to try again (Y/N)");
+            string input = Console.ReadLine();
+
+            if (input.ToUpper() != "Y")
+            {
+                again = false;
+            }
+        }
+        
+
+
+        Console.ReadKey();
+        
+    }
+}
